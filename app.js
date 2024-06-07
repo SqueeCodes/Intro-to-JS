2346accca2af00373e8139cf8bdbc87d5c894aef
David <itsmesquee@gmail.com>
2024-06-07 10:48:15 -0600

    utilizing comments

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ console.log('Hello World'[10])
 console.log('12345'.length)
 let str = 'David'
 console.log(str[5 - 1])
+// console log the numbers 1 to 3
 console.log('123')
+/**
+ * hi there
+ * comments appreciated!
+ */
