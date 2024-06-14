7cd557cf6ac7c2c6323ec7becc19b17f18932c0b
David <itsmesquee@gmail.com>
2024-06-14 13:57:59 -0600

    made for loop more effeciant

--- app.js
+++ app.js
@@ -103,14 +103,17 @@ console.log(str1);
 
 for (let i = 1; i <= 20 ; ++i ) {
     console.log(i);
-    if (i % 3 === 0) {
-     console.log(`${i} -> Frontend`)
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
+     console.log(`${i} -> frontend`)
     }
     else if (i % 5 === 0) {
      console.log(`${i} -> Simplified`)
     }
-    else (i % 3 === 0 && i % 5 === 0) {
-     console.log(`${i} -> Frontend Simplified`)
+    else {
+        console.log(`${i} -> ${i}`)
     }
 
 }
