3db647f76c92f3694910ae8c6430d43ed9f4d5ec
David <itsmesquee@gmail.com>
2024-06-14 08:31:33 -0600

    added logged else if script

--- app.js
+++ app.js
@@ -20,3 +20,19 @@ console.log(fahrenheit);
 
 let bool = '1' !== 1
 console.log(bool)
+
+/* CONDITIONALS */
+
+let subscribed = false
+let loggedIn = true
+
+if (subscribed === true) {
+    console.log('show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell the user to upgrade thier subscription')
+}
+else {
+    console.log('tell user to log into the account')
+
+}
