1e25d09b22e95fb55c70e72ae3da0333efc941da
David <itsmesquee@gmail.com>
2024-06-14 13:52:28 -0600

    fixed for loop bug

--- app.js
+++ app.js
@@ -106,10 +106,10 @@ for (let i = 1; i <= 20 ; ++i ) {
     if (i % 3 === 0) {
      console.log(`${i} -> Frontend`)
     }
-    if (i % 5 === 0) {
+    else if (i % 5 === 0) {
      console.log(`${i} -> Simplified`)
     }
-    if (i % 3 === 0 && i % 5 === 0) {
+    else (i % 3 === 0 && i % 5 === 0) {
      console.log(`${i} -> Frontend Simplified`)
     }
 
